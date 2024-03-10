04760bd6e5db94bdd50f8cf91aa156b1f5f49ade
Quan HL <quan.luuhoang8@gmail.com>
2024-03-10 13:02:32 +0700

    wip

--- mod_dub/audio_downloader.h
+++ mod_dub/audio_downloader.h
@@ -9,7 +9,7 @@ extern "C" {
 switch_status_t init_audio_downloader();
 switch_status_t deinit_audio_downloader();
 
-int start_audio_download(const char* url, int rate, int loop, int gain, switch_mutex_t* mutex, CircularBuffer_t* buffer);
+int start_audio_download(const char* url, const char* body, std::vector<std::string> headers, int rate, int loop, int gain, switch_mutex_t* mutex, CircularBuffer_t* buffer);
 switch_status_t stop_audio_download(int id);
 
 }
