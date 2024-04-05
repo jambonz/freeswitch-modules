8f1e81c8b6e03923aa238de1bf407c7e8c168849
Hoan HL <quan.luuhoang8@gmail.com>
2024-04-05 02:22:43 +0000

    remove deepgram tts linear16 wav 44 bytes header
    Signed-off-by: Hoan HL <quan.luuhoang8@gmail.com>

--- mod_deepgram_tts/deepgram_glue.cpp
+++ mod_deepgram_tts/deepgram_glue.cpp
@@ -421,6 +421,11 @@ static size_t write_cb(void *ptr, size_t size, size_t nmemb, ConnInfo_t *conn) {
 
   size_t numSamples = total_bytes_to_process / sizeof(int16_t);
   int16_t* inputData = reinterpret_cast<int16_t*>(data);
+  if (0 == d->reads++) {
+    fireEvent = true;
+    // Deepgram return PCM linear16 WAV file which contains 44 bytes headers, remove that.
+    inputData += 22;
+  }
   {
     switch_mutex_lock(d->mutex);
 
@@ -446,9 +451,6 @@ static size_t write_cb(void *ptr, size_t size, size_t nmemb, ConnInfo_t *conn) {
     /* Push the data into the buffer */
     cBuffer->insert(cBuffer->end(), inputData, inputData + numSamples);
 
-    if (0 == d->reads++) {
-      fireEvent = true;
-    }
     switch_mutex_unlock(d->mutex);
   }
   if (fireEvent && d->session_id) {
