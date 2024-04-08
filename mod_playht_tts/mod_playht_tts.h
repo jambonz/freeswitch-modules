ee488e4621cd238784a13662f0f6204807127931
Hoan Luu Huu <110280845+xquanluu@users.noreply.github.com>
2024-04-08 21:22:16 +0700

    support mod_playht_tts (#38)
    * support mod_playht_tts
    
    * wip
    
    Signed-off-by: Hoan HL <quan.luuhoang8@gmail.com>
    
    * wip
    
    Signed-off-by: Hoan HL <quan.luuhoang8@gmail.com>
    
    * wip
    
    Signed-off-by: Hoan HL <quan.luuhoang8@gmail.com>
    
    ---------
    
    Signed-off-by: Hoan HL <quan.luuhoang8@gmail.com>

---
+++ mod_playht_tts/mod_playht_tts.h
@@ -0,0 +1,41 @@
+#ifndef __MOD_PLAYHT_TTS_H__
+#define __MOD_PLAYHT_TTS_H__
+
+#include <switch.h>
+typedef struct playht_data {
+  char *voice_name;
+  char *api_key;
+  char *user_id;
+  char *quality;
+  char *speed;
+  char *seed;
+  char *temperature;
+  char *voice_engine;
+  char *emotion;
+  char *voice_guidance;
+  char *style_guidance;
+  char *text_guidance;
+
+  /* result data */
+  long response_code;
+  char *ct;
+  char *reported_latency;
+  char *request_id;
+  char *name_lookup_time_ms;
+  char *connect_time_ms;
+  char *final_response_time_ms;
+  char *err_msg;
+  char *cache_filename;
+  char *session_id;
+
+  int rate;
+  int draining;
+  int reads;
+  int cache_audio;
+
+	void *conn;
+  void *circularBuffer;
+  switch_mutex_t *mutex;
+  FILE *file;
+} playht_t;
+#endif
