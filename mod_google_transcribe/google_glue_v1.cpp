4bd6023f3e15d5e7b0dd1a7d6af49ebb8f1eb8c3
Andrew Golledge <andreas.golledge@gmail.com>
2024-04-22 08:49:38 +0200

    Correct error in JSON field name.

--- mod_google_transcribe/google_glue_v1.cpp
+++ mod_google_transcribe/google_glue_v1.cpp
@@ -340,11 +340,11 @@ static void *SWITCH_THREAD_FUNC grpc_read_thread(switch_thread_t *thread, void *
           cb->responseHandler(session, "no_audio", cb->bugname);
         }
       }
-      else if (status.error_code() != 0)  {
+      else if (status.error_code() != 0) {
         cJSON* json = cJSON_CreateObject();
         cJSON_AddStringToObject(json, "type", "error");
-        cJSON_AddStringToObject(json, "error_type", "stream_close");
-        cJSON_AddItemToObject(json, "error_cause", cJSON_CreateNumber(status.error_code()));
+        cJSON_AddStringToObject(json, "error_cause", "stream_close");
+        cJSON_AddItemToObject(json, "error_code", cJSON_CreateNumber(status.error_code()));
         cJSON_AddStringToObject(json, "error_message", status.error_message().c_str());
         char* jsonString = cJSON_PrintUnformatted(json);
         cb->responseHandler(session, jsonString, cb->bugname);
