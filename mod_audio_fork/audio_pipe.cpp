37709120aa77b9bbeb42b7f89ad971c8c57672ab
Dave Horton <daveh@beachdognet.com>
2024-07-17 07:45:16 -0400

    wip

--- mod_audio_fork/audio_pipe.cpp
+++ mod_audio_fork/audio_pipe.cpp
@@ -73,7 +73,7 @@ int AudioPipe::lws_callback(struct lws *wsi,
       break;
 
     case LWS_CALLBACK_WS_CLIENT_DROP_PROTOCOL:
-      lwsl_error("AudioPipe::lws_service_thread LWS_CALLBACK_WS_CLIENT_DROP_PROTOCOL\n");
+      lwsl_err("AudioPipe::lws_service_thread LWS_CALLBACK_WS_CLIENT_DROP_PROTOCOL\n");
       break;
 
     case LWS_CALLBACK_EVENT_WAIT_CANCELLED:
