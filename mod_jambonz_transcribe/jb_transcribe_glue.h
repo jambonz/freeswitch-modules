e68c7f6912d7721f8dc40383e43f2227fa7f058e
Hoan HL <quan.luuhoang8@gmail.com>
2024-08-07 07:22:15 +0000

    support jambonz transcribe with multiple sampling rate

--- mod_jambonz_transcribe/jb_transcribe_glue.h
+++ mod_jambonz_transcribe/jb_transcribe_glue.h
@@ -6,7 +6,7 @@ int parse_ws_uri(switch_channel_t *channel, const char* szServerUri, char* host,
 switch_status_t jb_transcribe_init();
 switch_status_t jb_transcribe_cleanup();
 switch_status_t jb_transcribe_session_init(switch_core_session_t *session, responseHandler_t responseHandler, 
-		uint32_t samples_per_second, uint32_t channels, char* lang, int interim, char* bugname, void **ppUserData);
+		uint32_t samples_per_second, int desiredSampling, uint32_t channels, char* lang, int interim, char* bugname, void **ppUserData);
 switch_status_t jb_transcribe_session_stop(switch_core_session_t *session, int channelIsClosing, char* bugname);
 switch_bool_t jb_transcribe_frame(switch_core_session_t *session, switch_media_bug_t *bug);
 
